307ac04299f6c1014e6e60cf900447721d4dbb66
ssolano15 <s.solano15@gmail.com>
2019-03-26 10:47:00 -0600

    C

---
+++ source/ALU.sv
@@ -0,0 +1,11 @@
+module alu(
+	input [15:0] A,B,
+	input [3:0] ALU_Sel,
+	output [15:0] ALU_Out,
+	output CarryOut
+	output Overflow
+)
+reg 7:0 ALU_Result;
+wire [16:0] tmp
+assign ALU_Out=ALU_result
+
